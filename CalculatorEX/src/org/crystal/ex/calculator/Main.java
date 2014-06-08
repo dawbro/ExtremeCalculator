e68bcd28ebf6eb2a3a5b2351829077ebf7ad242c
dawbro <Dawson@DawsonPC>
2014-06-08 09:39:50 -0700

    +code

--- CalculatorEX/src/org/crystal/ex/calculator/Main.java
+++ CalculatorEX/src/org/crystal/ex/calculator/Main.java
@@ -1,5 +1,15 @@
 package org.crystal.ex.calculator;
-
+/**
+ * @author Dawbro
+ * I created this project on 6/8/2014 at the time of 9:36AM.
+ * This is the CalculatorEX Program.
+ * This is a very complex program that uses a database and a server.
+ * Copyright © CrystalCoderz 2013-2014 All Rights Reserved.
+ */
 public class Main {
+	Gui g = new Gui();
+	public static void main(String[] args){
+		
+	}
 
 }
